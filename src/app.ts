d17abac4fa89d1ef209f3493cb49736d2b625c46
emiliobongiorno <emiliobongiornov@gmail.com>
2025-09-28 12:29:45 -0300

    Add swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import v1Routes from "./routes/v1.routes"
 import v2Routes from "./routes/v2.routes"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from './swagger.json';
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', v1Routes)
 app.use('/api/v2', v2Routes)
 
